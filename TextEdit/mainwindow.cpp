68e1ebb84133c80d63d501a6b87014a51c1f0d07
LiuZichen <danteliuzichen@163.com>
2018-07-13 11:58:38 +0800

    实现基本功能

--- TextEdit/mainwindow.cpp
+++ TextEdit/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
+#include <QFileDialog>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -35,4 +37,81 @@ void MainWindow::show_findText(QString str)
         QMessageBox::information(this,tr("注意"),tr("没有找到内容"),QMessageBox::Ok);
         ui->textEdit->moveCursor(QTextCursor::Start, QTextCursor::MoveAnchor); //将光标置于开始位置
     }
+}
+
+//文件另存为
+void MainWindow::on_action_A_triggered()
+{
+    if(ui->textEdit->toPlainText()==NULL)
+    {
+        QMessageBox::information(this,tr("注意"),tr("当前文本没有内容"),QMessageBox::Ok);
+    }
+    else {
+        QString fileName = QFileDialog::getSaveFileName(
+                        this, tr("save txt file"),
+                        "/", tr("Txt files(*.txt)"));
+        QFile f1(fileName);
+        f1.open(QIODevice::WriteOnly|QIODevice::Text);
+        QTextStream str(&f1);
+        str<<ui->textEdit->toPlainText();
+        f1.close();
+    }
+}
+
+//打开新文件
+void MainWindow::on_action_O_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(
+                    this, tr("open txt file"),
+                    "/", tr("Txt files(*.txt)"));
+    QString displayString;
+    QFile file(fileName);
+    if(!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        qDebug()<<"Can't open the file!"<<endl;
+    }
+    while(!file.atEnd())
+    {
+        QTextStream txtInput(&file);
+        QString str=txtInput.readLine();
+        displayString.append(str);
+    }
+    ui->textEdit->clear();
+    ui->textEdit->setPlainText(displayString);
+    myfile=fileName;
+}
+
+//保存文件
+void MainWindow::on_action_S_triggered()
+{
+    if(myfile==NULL)
+    {
+        QString fileName = QFileDialog::getSaveFileName(
+                        this, tr("save txt file"),
+                        "/", tr("Txt files(*.txt)"));
+        myfile=fileName;
+    }
+    QFile f1(myfile);
+    f1.open(QIODevice::WriteOnly|QIODevice::Text);
+    QTextStream str(&f1);
+    str<<ui->textEdit->toPlainText();
+    f1.close();
+}
+
+//新建文件
+void MainWindow::on_action_N_triggered()
+{
+    myfile.clear();
+    ui->textEdit->clear();
+    QString fileName = QFileDialog::getSaveFileName(
+                    this, tr("new txt file"),
+                    "/", tr("Txt files(*.txt)"));
+    myfile=fileName;
+}
+
+//关闭文件
+void MainWindow::on_action_C_triggered()
+{
+    myfile.clear();
+    ui->textEdit->clear();
 }
--- TextEdit/mainwindow.h
+++ TextEdit/mainwindow.h
@@ -16,12 +16,23 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     SearchDialog *sdlg;
+    QString myfile;
 
 public slots:
     void show_findText(QString str);
 
 private slots:
     void on_action_W_triggered();
+
+    void on_action_A_triggered();
+
+    void on_action_O_triggered();
+
+    void on_action_S_triggered();
+
+    void on_action_N_triggered();
+
+    void on_action_C_triggered();
 
 private:
     Ui::MainWindow *ui;
